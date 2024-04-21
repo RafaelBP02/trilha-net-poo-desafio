3bd98824a6aad083bc2c43eb67a03d4b427f4cbc
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-04-20 22:11:39 -0300

    Feat: Implementa classe Iphone segundo o diagrama de classes

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,15 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string nome, string modelo, string imei, int memoria)
+                                        : base(nome, modelo, imei, memoria) { }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando app {nomeApp} de IPhone {Modelo}, IMEI {IMEI} e {Memoria}MB de memoria");
+        }
     }
 }
--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,16 +2,19 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-        string Modelo { get; set; }
-        string IMEI { get; set; }
-        int Memoria { get; set; }
+
+        public string numero; //Se a propriedade eh publica, nao vejo necessidade de utilizat get set
+        
+        // Essas props automaticas abaixo, no meu entendimento, implementam por baixo dos panos uma propriedade privada.
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public int Memoria { get; set; }
 
         public Smartphone(string numero, string modelo, string imei, int memoria)
         {
-            Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            this.numero = numero;
             Modelo = modelo;
             IMEI = imei;
             Memoria = memoria;
