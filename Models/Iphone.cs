44563d090afcbcff248323cb7f7175517c5764ed
RafaelBP02 <rafaelbertopereira@gmail.com>
2024-04-20 22:19:28 -0300

    debug: Corrige bug da classe Iphone

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -3,8 +3,8 @@ namespace DesafioPOO.Models
     // TODO: Herdar da classe "Smartphone"
     public class Iphone : Smartphone
     {
-        public Iphone(string nome, string modelo, string imei, int memoria)
-                                        : base(nome, modelo, imei, memoria) { }
+        public Iphone(string numero, string modelo, string imei, int memoria)
+                                        : base(numero, modelo, imei, memoria) { }
 
         // TODO: Sobrescrever o método "InstalarAplicativo"
         public override void InstalarAplicativo(string nomeApp)
--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿using DesafioPOO.Models;
 
+Iphone meuFone = new Iphone("91234-5432", "15 Platinum Pro Max", "algo XYZ", 256 );
+
+meuFone.InstalarAplicativo("Doodle Jump");
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
